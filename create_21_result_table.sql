8eafebce47c83845bfdf2480c094ed820e5c8dfa
sxtyzy200000104 <29521614@qq.com>
2019-07-15 07:30:23 +0800

    需要按实际更换期间编号的成母牛21天妊娠率算法和待修改的青年牛算法(情期初节点和情期末节点确定，之后代码需要修改)

---
+++ create_21_result_table.sql
@@ -0,0 +1,4 @@
+
+#成母牛的结果表
+create table 21_result(id int primary key auto_increment,start_date date,end_date date,should_be_bred_cnt int,bred_cnt int ,preg_cnt int,first_should_be_bred_cnt int,first_bred_cnt int ,first_preg_cnt int,delivered_should_be_bred_cnt int,delivered_bred_cnt int ,delivered_preg_cnt int);
+
---
+++ create_21_young_result_table.sql
@@ -0,0 +1,2 @@
+#青年牛的结果表
+create table 21_young_result(id int primary key auto_increment,start_date date,end_date date,should_be_bred_cnt int,bred_cnt int ,preg_cnt int,first_should_be_bred_cnt int);
